e2e5d0581dac7f0720ef8e79b3c2008eb3124e8d
renatakov <renatakov1010@gmail.com>
2023-10-23 17:38:47 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Header from "./assets/Header/Header";
-
+import Home from "./components/Home/Home";
 const App = () => {
   return(
+    <>
+    
     <Header/>
+    <Home/>
+    </>
   )
 }
 
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useState } from "react";
+
+const Home = () => {
+    // const [popularRecipie, setPopularRecipie] = useState([]);
+    // const apiKey = ""
+    // const getPopularMeal = async () => {
+
+    //     await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=1&tags=main course`)
+    //     .then((res)=>{
+            
+
+    //     })
+    // }
+    // getPopularMeal()
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default Home
